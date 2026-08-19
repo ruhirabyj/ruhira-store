708457af2c0ba27823dabd3e79bde2770576cb29
ruhirabyj <ruhirabyj@gmail.com>
2026-08-19 18:58:22 +0530

    Update product.js

--- product.js
+++ product.js
@@ -1,69 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     /* =========================================
-       PRODUCT DATABASE
-    ========================================= */
-
-    const products = {
-
-        RKS101: {
-            id: "RKS101",
-            name: "Pink Bloom",
-            price: 2199,
-
-            images: [
-                "https://res.cloudinary.com/kipwuap2/image/upload/v1787125875/IMG_1424.jpg",
-                "https://res.cloudinary.com/kipwuap2/image/upload/v1787125875/IMG_1423.jpg",
-                "https://res.cloudinary.com/kipwuap2/image/upload/v1787125875/IMG_1407.jpg",
-                "https://res.cloudinary.com/kipwuap2/image/upload/v1787125875/IMG_1403.jpg",
-                "https://res.cloudinary.com/kipwuap2/image/upload/v1787125875/IMG_1343.jpg"
-            ]
-        },
-
-        RKS102: {
-            id: "RKS102",
-            name: "Forest Grove",
-            price: 2199,
-
-            images: [
-                "https://res.cloudinary.com/kipwuap2/image/upload/v1787125243/IMG_1749.jpg"
-            ]
-        },
-
-        RKS103: {
-            id: "RKS103",
-            name: "Amber Glow",
-            price: 2199,
-
-            images: [
-                "https://res.cloudinary.com/kipwuap2/image/upload/v1787125256/IMG_1794.jpg"
-            ]
-        },
-
-        RKS104: {
-            id: "RKS104",
-            name: "Midnight Noir",
-            price: 2299,
-
-            images: [
-                "https://res.cloudinary.com/kipwuap2/image/upload/v1787125275/IMG_1636.jpg"
-            ]
-        },
-
-        RKS105: {
-            id: "RKS105",
-            name: "Ivory Mist",
-            price: 2799,
-
-            images: [
-                "https://res.cloudinary.com/kipwuap2/image/upload/v1787125294/IMG_1577.jpg"
-            ]
-        }
-
-    };
-
-
-    /* =========================================
        GET SELECTED PRODUCT
     ========================================= */
 
